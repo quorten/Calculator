a1f204d91ba8d2b30e647e5ab1a46c3b2ac747e1
Andrew Makousky <mako0042@umn.edu>
2013-06-12 23:17:18 -0500

    Initial commit.

---
+++ Calculator.java
@@ -0,0 +1,222 @@
+// Calculator.java -- A simple Java calculator.
+// Copyright (C) 2013 Andrew Makousky
+
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use, copy,
+// modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class Calculator implements ActionListener
+{
+    private JFrame frame;
+    private JTextArea textView;
+
+    private double lastNumber, curNumber;
+    private char lastOp;
+    private boolean afterDec;
+    private int decPlace;
+    private boolean eqPressed;
+
+    public Calculator()
+    {
+	// Setup the look and feel.
+	String lookAndFeel = UIManager.getSystemLookAndFeelClassName();
+	try {
+	    UIManager.setLookAndFeel(lookAndFeel);
+	}
+	catch (Exception e) {
+	}
+
+	// Setup the main window.
+	frame = new JFrame("Calculator");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	GridBagLayout gridbag = new GridBagLayout();
+	GridBagConstraints c = new GridBagConstraints();
+	c.insets = new Insets(2, 2, 2, 2);
+	c.fill = GridBagConstraints.BOTH;
+
+	JPanel panel = new JPanel(gridbag);
+	panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+	frame.add(panel, BorderLayout.CENTER);
+
+	// Add the numeric buttons.
+	JButton[] buttons = new JButton[18];
+	int i;
+	for (i = 0; i < 10; i++) {
+	    buttons[i] = new JButton("" + i);
+	    // Warning: shortcut to set key mnemonic
+	    buttons[i].setMnemonic(KeyEvent.VK_0 + i);
+	    buttons[i].addActionListener(this);
+	    if (i != 0) {
+		c.gridx = (i - 1) % 3;
+		c.gridy = 3 - (i - 1) / 3;
+		gridbag.setConstraints(buttons[i], c);
+		panel.add(buttons[i]);
+	    }
+	    else {
+		c.gridx = 0; c.gridy = 4;
+		gridbag.setConstraints(buttons[i], c);
+		panel.add(buttons[i]);
+	    }
+	}
+
+	// Add the function keys.
+	String[] funcLabels = { ".", "+", "-", "*", "/", "=", "C", "CE" };
+	int[] funcGxPos     = {  1,   3,   4,   3,   4,   3,   3,   4 };
+	int[] funcGyPos     = {  4,   3,   3,   2,   2,   4,   1,   1 };
+	int[] funcMnems = { KeyEvent.VK_PERIOD, KeyEvent.VK_PLUS,
+			    KeyEvent.VK_MINUS, KeyEvent.VK_ASTERISK,
+			    KeyEvent.VK_SLASH, KeyEvent.VK_EQUALS,
+			    KeyEvent.VK_C, KeyEvent.VK_E };
+	c.gridwidth = 1; c.gridheight = 1;
+	while (i < 18) {
+	    c.gridx = funcGxPos[i-10];
+	    c.gridy = funcGyPos[i-10];
+	    if (funcLabels[i-10].equals("="))
+		c.gridwidth = 2;
+	    else
+		c.gridwidth = 1;
+	    buttons[i] = new JButton(funcLabels[i-10]);
+	    buttons[i].setMnemonic(funcMnems[i-10]);
+	    buttons[i].addActionListener(this);
+	    gridbag.setConstraints(buttons[i], c);
+	    panel.add(buttons[i]);
+	    i++;
+	}
+
+	// Add the calculator screen.
+	c.gridx = 0; c.gridy = 0;
+	c.gridwidth = 5; c.gridheight = 1;
+	textView = new JTextArea();
+	textView.setEditable(false);
+	gridbag.setConstraints(textView, c);
+	panel.add(textView);
+
+	// Display the window.
+	frame.pack();
+	frame.setVisible(true);
+
+	// Initialize the calculator state.
+	initState();
+	textView.setText("" + curNumber);
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+	String btnName = ((JButton)e.getSource()).getText();
+	char btnChar = btnName.charAt(0);
+	switch (btnChar) {
+
+	case '0': case '1': case '2': case '3': case '4': case '5':
+	case '6': case '7': case '8': case '9':
+	    if (eqPressed)
+		initState();
+	    if (!afterDec) {
+		curNumber = curNumber * 10.0 +
+		    Double.parseDouble(btnName);
+	    }
+	    else {
+		double digit = Double.parseDouble(btnName);
+		for (int i = 0; i < decPlace; i++)
+		    digit /= 10.0;
+		curNumber = curNumber + digit;
+		decPlace++;
+	    }
+	    textView.setText("" + curNumber);
+	    break;
+
+	case '.':
+	    if (eqPressed)
+		initState();
+	    afterDec = true;
+	    textView.setText("" + curNumber);
+	    break;
+
+	case '+':
+	case '-':
+	case '*':
+	case '/':
+	    if (eqPressed)
+		eqPressed = false;
+	    else
+		compute();
+	    lastOp = btnChar;
+	    curNumber = 0.0;
+	    textView.setText("" + lastNumber);
+	    break;
+
+	case '=':
+	    eqPressed = true;
+	    compute();
+	    textView.setText("" + lastNumber);
+	    break;
+
+	case 'C':
+	    if (btnName.length() == 1)
+		initState();
+	    else {
+		eqPressed = false;
+		curNumber = 0.0;
+	    }
+	    textView.setText("" + curNumber);
+	    break;
+	}
+    }
+
+    private void initState()
+    {
+	lastNumber = curNumber = 0.0;
+	lastOp = '\0';
+	afterDec = false;
+	decPlace = 1;
+	eqPressed = false;
+    }
+
+    private void compute()
+    {
+	switch (lastOp) {
+	case '\0':
+	    lastNumber = curNumber;
+	    break;
+	case '+':
+	    lastNumber = lastNumber + curNumber;
+	    break;
+	case '-':
+	    lastNumber = lastNumber - curNumber;
+	    break;
+	case '*':
+	    lastNumber = lastNumber * curNumber;
+	    break;
+	case '/':
+	    lastNumber = lastNumber / curNumber;
+	    break;
+	}
+	afterDec = false;
+	decPlace = 1;
+    }
+
+    public static void main(String[] args)
+    {
+	Calculator calc = new Calculator();
+    }
+}
